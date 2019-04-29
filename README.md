e94e4e0ad2e7e34572fbcc64e73deb081d0962e1
Justin Kaufman <jmkauf@gmail.com>
2019-04-29 16:35:44 -0700

    seems to work

---
+++ README.md
@@ -0,0 +1,10 @@
+# StoryFramework [![Carthage compatible](https://img.shields.io/badge/Carthage-compatible-4BC51D.svg?style=flat)](https://github.com/Carthage/Carthage)
+
+Kotlin Multiplatform Framework for the Story iOS application.
+
+## Installation
+
+### Carthage
+```
+github "JUSTINMKAUFMAN/StoryFramework"
+```
---
+++ StoryFramework/Supporting Files/StoryFramework.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for StoryFramework.
+FOUNDATION_EXPORT double StoryFrameworkVersionNumber;
+
+//! Project version string for StoryFramework.
+FOUNDATION_EXPORT const unsigned char StoryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StoryFramework/PublicHeader.h>
